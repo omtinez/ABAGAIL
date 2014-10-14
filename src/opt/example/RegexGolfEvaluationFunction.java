258e360817eea5b8fc775963bef88fcb167b8ad9
omtinez <omtinez@gmail.com>
2014-10-13 19:08:43 -0700

    Fix initialization and evaluation routines

--- src/opt/example/RegexGolfEvaluationFunction.java
+++ src/opt/example/RegexGolfEvaluationFunction.java
@@ -1,5 +1,6 @@
 package opt.example;
 
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
@@ -26,24 +27,55 @@ public class RegexGolfEvaluationFunction implements EvaluationFunction {
     private String[] matchNegList;
     
     /**
+     * The set of regex building blocks to be used
+     */
+    private String[] rexComponents;
+    
+    /**
      * Map a given double to a typeable character. 
      * Instead of dealing with probability distributions, 
      * we make some chars like letters more likely here 
      */
-    private char mapDoubleToChar(double d) {
+    public static char mapDoubleToChar(double d) {
     	if (d < 32.0) return '\0';
     	else if (d > 126.0) return (char)(((int) d) % 25 + 97);
     	else return (char)((int) d);
     }
     
     /**
+     * Map the given parameter vector to regex string
+     */
+    public String mapParamsToRegex(Instance d) {
+    	Vector data = d.getData();
+    	StringBuilder sb = new StringBuilder();
+    	for (int i = 0; i < data.size(); i++) {
+    		if (data.get(i) > 0 && rexComponents[i] != null) {
+    			sb.append("|").append(rexComponents[i]);
+    		}
+    	}
+    	
+        return sb.length() == 0 ? "" : sb.toString().substring(1);
+    }
+    
+    /**
      * Make a new regex golf evaluation function
      * @param p the set of words that we must match (positive)
      * @param n the set of words that we shouldn't match (negative)
+     * @param c the set of regex building blocks to be used
      */
-    public RegexGolfEvaluationFunction(String[] p, String[] n) {
+    public RegexGolfEvaluationFunction(String[] p, String[] n, String[] c) {
     	matchPosList = p;
     	matchNegList = n;
+    	rexComponents = c;    	
+    	for(int i = 0; i < rexComponents.length; i++) {
+    		Matcher m = Pattern.compile(rexComponents[i]).matcher("");
+    		for (String s : matchNegList) {
+    			if (m.reset(s).find()) {
+    				rexComponents[i] = null;
+    				break;
+    			}
+    		}
+    	}
     }
 
     /**
@@ -51,26 +83,11 @@ public class RegexGolfEvaluationFunction implements EvaluationFunction {
      */
     public double value(Instance d) {
     	int value = 0;
-        Vector data = d.getData();
-        
-        // First unit in vector tells us index of word
-        String w = matchPosList[(int) (matchPosList.length * data.get(0) / 256)];
-        
-        // Second and third units tell us start/end substr index
-        int sub0 = (int) ((w.length() - 1) * data.get(1) / 256);
-        int sub1 = (int) ((w.length() - 1 - sub0) * data.get(2) / 256) + 1 + sub0;
-
-        // Fourth unit tells us index of character to replace
-        int ix = (int) ((sub1 - sub0) * data.get(3) / 256);
-        
-        // Fifth unit tells us character to replace by
-        char c = mapDoubleToChar(data.get(4));
-        StringBuilder sb = new StringBuilder();        
-        sb.append(w.substring(sub0, sub1)).replace(ix, ix, String.valueOf(c));
+        String pat = mapParamsToRegex(d);
         
         Matcher m;
         try {
-        	m = Pattern.compile(sb.toString()).matcher("");
+        	m = Pattern.compile(pat).matcher("");
         } catch (PatternSyntaxException e) {
         	return (double) Integer.MIN_VALUE;
         }
@@ -80,6 +97,6 @@ public class RegexGolfEvaluationFunction implements EvaluationFunction {
         for (String s : matchNegList) {
         	if (m.reset(s).find()) value -= 10;
         }
-        return (double) value - sb.length();
+        return (double) value - pat.length();
     }
 }
